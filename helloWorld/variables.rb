06ca74a32282a954809cafd4bf82d3e2c52fad77
jfayerz <jfayerz+git@icloud.com>
2018-08-20 22:12:33 -0400

    added a few other beginner ruby scripts

---
+++ helloWorld/variables.rb
@@ -0,0 +1,22 @@
+# local variables and global variables
+
+localvar = "hello"
+$globalvar = "goodbye"
+
+def amethod
+  localvar = 10
+  puts( localvar )
+  puts ($globalvar)
+end
+
+def anotherMethod
+  localvar = 500
+  $globalvar = "bonjour"
+  puts( localvar)
+  puts($globalvar)
+end
+
+amethod
+anotherMethod
+puts localvar
+puts $globalvar
